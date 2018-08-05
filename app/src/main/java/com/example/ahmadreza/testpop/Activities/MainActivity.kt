49395902eea05fb83a79f1b2d6b4b8bc8684812f
ahmadreza <moodi1378@gmail.com>
2018-08-05 22:39:34 +0430

    add smarttab and view pager every thing is OK

--- app/src/main/java/com/example/ahmadreza/testpop/Activities/MainActivity.kt
+++ app/src/main/java/com/example/ahmadreza/testpop/Activities/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.ahmadreza.testpop.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import com.example.ahmadreza.testpop.Adaptors.ViewPageAdaptor
 import com.example.ahmadreza.testpop.Data.DownloadWebContent
 import com.example.ahmadreza.testpop.Data.Server
+import com.example.ahmadreza.testpop.Fragments.Recent
 import com.example.ahmadreza.testpop.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +19,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var toolbar = supportActionBar
+
+        var viewPad = ViewPageAdaptor(supportFragmentManager)
+
+        viewPad.addFragment(Recent(), "Recent")
+
+        viewpager.adapter = viewPad
+        tab_View_pager.setViewPager(viewpager)
 
         val server = Server.st
 
--- app/src/main/java/com/example/ahmadreza/testpop/Adaptors/ViewPageAdaptor.kt
+++ app/src/main/java/com/example/ahmadreza/testpop/Adaptors/ViewPageAdaptor.kt
@@ -1,7 +1,33 @@
 package com.example.ahmadreza.testpop.Adaptors
 
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
 /**
  * Created by ahmadreza on 8/5/18.
  */
-class ViewPageAdaptor {
+class ViewPageAdaptor(fm: FragmentManager): FragmentPagerAdapter(fm) {
+
+    private val fragmentList = ArrayList<Fragment>()
+    private val fragmentTitle = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment {
+        return fragmentList.get(position)
+    }
+
+    override fun getCount(): Int {
+        return fragmentTitle.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return fragmentTitle.get(position)
+    }
+
+    fun addFragment(fragment: Fragment, title: String): Unit {
+        fragmentList.add(fragment)
+        fragmentTitle.add(title)
+    }
+
+
 }
